5eabef20a7e6b0d76a83d3e1c0182c1539ad9794
Reinert Huseby Karlsen <reinert.karlsen@gmail.com>
2022-10-24 08:48:10 +0200

    fix new api url and refactoring

--- src/sondera/clients/sgu/groundwater.py
+++ src/sondera/clients/sgu/groundwater.py
@@ -22,19 +22,28 @@ from ..parameters import ParametersGWLevels as Parameters
 
 class GroundwaterLevels:
     _api_url = 'https://resource.sgu.se/oppnadata/grundvatten/api/grundvattennivaer'
+    _api_url_v1 = 'https://resource.sgu.se/oppnadata/grundvatten/grundvattennivaer'
 
     def __init__(self):
 
         # self.Parameters = Parameters
 
+        # Returns all observations for a station-id
         self._api_url_template_data = (self._api_url +
                                        '/nivaer/station'
                                        '/{station}?format=json')
 
+        # Returns all observations for all stations in a specified län
         self._api_url_template_data_lan = (self._api_url +
-                                           '/nivaer/lan'
-                                           '/{lancode}?format=json')
+                                           '/nivaer/v1/lan/{lancode}'
+                                           '?format=json')
 
+        # Old format, deprecated
+        # self._api_url_template_data_lan = (self._api_url +
+        #                                    '/nivaer/lan'
+        #                                    '/{lancode}?format=json')
+
+        # List of stations available for each län
         self._api_url_template_stations_lan = (self._api_url +
                                                '/stationer'
                                                '/{lancode}?format=json')
@@ -72,43 +81,47 @@ class GroundwaterLevels:
 
         station_name = api_data['features'][0]['properties']['stationens_namn']
         # convert coordinates to WGS84
-        station_md = {}
-        station_md['crs'] = api_data['crs']['properties']['name']
-        station_md['coordinates'] = api_data['features'][0]['geometry']['coordinates']
-        station_md['key'] = api_data['features'][0]['properties']['omrade-_och_stationsnummer']
-
-        # station labeled active if last data is less than 30 days old
-        station_md['active'] = (pd.Timestamp.today() - obs_s.index.max()).days < 30
-        station_md['active_period'] = [obs_s.index.min(), obs_s.index.max()]
+        station_md = {'crs': api_data['crs']['properties']['name'],
+                      'coordinates': api_data['features'][0]['geometry']['coordinates'],
+                      'key': api_data['features'][0]['properties']['omrade-_och_stationsnummer'],
+                      'active': (pd.Timestamp.today() - obs_s.index.max()).days < 90,
+                      'active_period': [obs_s.index.min(), obs_s.index.max()]}
 
         # TODO: can lancode be taken from kommunkod? first two characters
         # can be used to query for stations nearby
 
-        # FIXME DRY: code block repeated
         # station can have key 'referensniva_for_roroverkant_m_o.h.' or
         #                      'referensniva_for_roroverkant_m.o.h.'
         # for datum level, only one can be present for each station
-        # only one will be present for each station
-        ref_datum_level_key = list(
-            set(['referensniva_for_roroverkant_m_o.h.', 'referensniva_for_roroverkant_m.o.h.']).intersection(
-                api_data['features'][0]['properties'].keys()))[0]
+        # only one will be present for each station, sometimes none
+        api_data_s = api_data['features'][0]
+        station_md = self._add_station_info(api_data_s, station_md)
+
+        return self._create_data_obj(aux_df, obs_s, parameter, station_md, station_name, md_str)
+
+    def _add_station_info(self, api_data_s, station_md):
+        try:
+            ref_datum_level_key = list({'referensniva_for_roroverkant_m_o.h.', 'referensniva_for_roroverkant_m.o.h.'}
+                                       .intersection(api_data_s['properties'].keys()))[0]
+
+        except IndexError:
+            ref_datum_level_key = None
 
         station_md['station_info'] = {
-            'start_date': api_data['features'][0]['properties'].get('startdatum_for_matning', None),
-            'aquifer_type': api_data['features'][0]['properties'].get('akvifertyp', None),
-            'topographic_position': api_data['features'][0]['properties'].get('topografiskt_lage', None),
-            'reference_datum_well_top': api_data['features'][0]['properties'].get(ref_datum_level_key,
-                                                                                  None),
-            'well_elev_above_ground': api_data['features'][0]['properties'].get('rorhojd_ovan_mark_m', None),
-            'well_length': api_data['features'][0]['properties'].get('total_rorlangd_m', None),
-            'municipality': api_data['features'][0]['properties'].get('kommunkod', None),
-            'eucd_groundwater_resource': api_data['features'][0]['properties'].get('eucd_far_grundvattenforekomst',
-                                                                                   None),
-            'measurement_quality': api_data['features'][0]['properties'].get('nivamatningskvalitet', None)}
+            'start_date': api_data_s['properties'].get('startdatum_for_matning', None),
+            'soil_type': api_data_s['properties'].get('jordart', None),
+            'aquifer_type': api_data_s['properties'].get('akvifertyp', None),
+            'topographic_position': api_data_s['properties'].get('topografiskt_lage', None),
+            'reference_datum_well_top': api_data_s['properties'].get(ref_datum_level_key,
+                                                                                    None),
+            'well_elev_above_ground': api_data_s['properties'].get('rorhojd_ovan_mark_m', None),
+            'well_length': api_data_s['properties'].get('total_rorlangd_m', None),
+            'municipality': api_data_s['properties'].get('kommunkod', None),
+            'eucd_groundwater_resource': api_data_s['properties'].get('eucd_far_grundvattenforekomst',
+                                                                                     None),
+            'measurement_quality': api_data_s['properties'].get('nivamatningskvalitet', None)}
 
-        station_data = self._create_data_obj(aux_df, obs_s, parameter, station_md, station_name, md_str)
-
-        return station_data
+        return station_md
 
     def get_observations_lan(self, lan_code: Union[SGULanCodes, str]):
         # can probably share a lot with above get_station_data
@@ -135,29 +148,41 @@ class GroundwaterLevels:
                 s_x = None
                 s_y = None
 
+            s_d = {'X': s_x,
+                   'Y': s_y,
+                   'code': s['properties'].get('omrade-_och_stationsnummer', None),}
+
+            station_info = {}
+            station_info = self._add_station_info(s, station_info)
+            # FIXME Future. 3.9 merge dicts with |
+            s_d = {**s_d, **station_info['station_info']}
+
             # station can have key 'referensniva_for_roroverkant_m_o.h.' or
             #                      'referensniva_for_roroverkant_m.o.h.'
             # for datum level, only one can be present for each station
-            # only one will be present for each station
-            ref_datum_level_key = list(
-                set(['referensniva_for_roroverkant_m_o.h.', 'referensniva_for_roroverkant_m.o.h.']).intersection(
-                    s['properties'].keys()))[0]
-
-            s_d = {'X': s_x,
-                   'Y': s_y,
-                   'code': s['properties'].get('omrade-_och_stationsnummer', None),
-                   'name': s['properties'].get('stationens_namn', None),
-                   'start_date': s['properties'].get('startdatum_for_matning', None),
-                   'aquifer_type': s['properties'].get('akvifertyp', None),
-                   'topographic_position': s['properties'].get('topografiskt_lage', None),
-                   'reference_datum_well_top': s['properties'].get(ref_datum_level_key, None),
-                   'well_elev_above_ground': s['properties'].get('rorhojd_ovan_mark_m', None),
-                   'well_length': s['properties'].get('total_rorlangd_m', None),
-                   'municipality': s['properties'].get('kommunkod', None),
-                   'eucd_groundwater_resource': s['properties'].get('eucd_far_grundvattenforekomst', None),
-                   'measurement_quality': s['properties'].get('nivamatningskvalitet', None)
-                   }
-            stations_df = stations_df.append(pd.Series(s_d, name=s_d['code']))
+            # only one will be present for each station, sometimes none
+            # try:
+            #     ref_datum_level_key = list({'referensniva_for_roroverkant_m_o.h.', 'referensniva_for_roroverkant_m.o.h.'}
+            #                                .intersection(s['properties'].keys()))[0]
+            # except IndexError:
+            #     ref_datum_level_key = None
+            #
+            # s_d = {'X': s_x,
+            #        'Y': s_y,
+            #        'code': s['properties'].get('omrade-_och_stationsnummer', None),
+            #        'name': s['properties'].get('stationens_namn', None),
+            #        'start_date': s['properties'].get('startdatum_for_matning', None),
+            #        'soil_type': s['properties'].get('jordart', None),
+            #        'aquifer_type': s['properties'].get('akvifertyp', None),
+            #        'topographic_position': s['properties'].get('topografiskt_lage', None),
+            #        'reference_datum_well_top': s['properties'].get(ref_datum_level_key, None),
+            #        'well_elev_above_ground': s['properties'].get('rorhojd_ovan_mark_m', None),
+            #        'well_length': s['properties'].get('total_rorlangd_m', None),
+            #        'municipality': s['properties'].get('kommunkod', None),
+            #        'eucd_groundwater_resource': s['properties'].get('eucd_far_grundvattenforekomst', None),
+            #        'measurement_quality': s['properties'].get('nivamatningskvalitet', None)
+            #        }
+            stations_df = pd.concat([stations_df, pd.Series(s_d, name=s_d['code'])], axis=1)
 
         return stations_df
 
@@ -167,6 +192,7 @@ class GroundwaterLevels:
 
     def _create_data_obj(self, aux_df, obs_s, parameter,
                          station_md, station_name, md_str):
+        # sourcery skip: inline-immediately-returned-variable
 
         pos_coord = Coordinate(x=station_md['coordinates'][0],
                                y=station_md['coordinates'][1],
@@ -180,8 +206,8 @@ class GroundwaterLevels:
                               station_type=StationType.GWStation,
                               active_station=station_md['active'],
                               active_period=station_md['active_period'],
-                              last_updated=None,
-                              station_info={})
+                              last_updated=obs_s.index.max(),
+                              station_info=station_md.get('station_info', None))
 
         data_obj = DataSeries(station=station_obj,
                               data=obs_s,
